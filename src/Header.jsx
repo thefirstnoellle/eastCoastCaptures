1592b215060aa62f1ee6524142910459af0c1f1a
thefirstnoellle <simmsnoelle@gmail.com>
2024-07-14 10:41:20 -0230

    import font

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './assets/2.png';
+import logo from './assets/3.png';
 
 function Header() {
   return (
     <nav>
       <ul>
         <li><a href="#home">about</a></li>
-        <li><a href="#about">galleries</a></li>
+        <li><a href="#about">services</a></li>
         <img src={logo} alt="Logo"/>
-        <li><a href="#services">pricing</a></li>
+        <li><a href="#services">galleries</a></li>
         <li><a href="#contact">contact</a></li>
       </ul>
     </nav>
