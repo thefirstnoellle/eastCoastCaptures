36f4531887dcaa0199cd6bd56dc5554258450ef1
thefirstnoellle <simmsnoelle@gmail.com>
2024-07-24 14:44:44 -0230

    about me image

--- src/About.jsx
+++ src/About.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function About() {
   return (
     <div className='aboutContainer'>
-      <h1 className='aboutTitle'>Hi, I'm Noelle</h1>
-      <div className="placeholderImg">placeholder image</div>
+      <h1 className='aboutTitle'>Hi! I'm Noelle</h1>
+      <img src="https://logos.pixieset.com/4660912/7e39ce59d6d0a36bbb95df38ddc21795-small.JPG" alt="Noelle" className="aboutIMG"/>
       <p className='aboutBody'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut culpa non debitis accusantium eveniet adipisci, quo ullam sit modi saepe ducimus quia aperiam perspiciatis. Fuga odit nesciunt inventore magni dolore? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quibusdam maiores aliquid sapiente, debitis ad itaque? Nobis deserunt saepe laborum totam voluptates non. Ipsa praesentium obcaecati voluptatum, possimus deserunt architecto?</p>
       </div>
 
